fb914f26990087d0550440206ffd9516065d3f76
Xavier Timbeau <xavier.timbeau@sciencespo.fr>
2025-03-10 12:23:24 +0100

    Update prix_insee.R

--- inst/ipch/prix_insee.R
+++ inst/ipch/prix_insee.R
@@ -1,5 +1,6 @@
 library(insee)
 library(dplyr)
+library(lubridate)
 
 ipchm <- get_idbank_list("IPCH-2015") |>
   filter(COICOP2016=="00", FREQ=="M", NATURE=="INDICE") |>
@@ -8,12 +9,12 @@ ipchm <- get_idbank_list("IPCH-2015") |>
   select(DATE, ipch = OBS_VALUE, IDBANK)
 
 ipch <- ipchm |>
-  mutate(DATE = lubridate::floor_date(DATE, unit="quarter")) |>
+  mutate(DATE = floor_date(DATE, unit="quarter")) |>
   group_by(DATE) |>
   summarise(ipch = mean(ipch))
 
 ipcha <- ipch |>
-  mutate(y = lubridate::year(DATE)) |>
+  mutate(y = year(DATE)) |>
   group_by(y) |>
   summarize(ipch = mean(ipch)) |>
   mutate(ipch = ipch / ipch[y == 2023])
